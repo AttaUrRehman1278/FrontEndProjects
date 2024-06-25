d421fa0dfc1ef0d636b791ace1afe97f75d1cf20
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-26 00:49:14 +0500

    CodePen

---
+++ codePen/src/components/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import Editor from './Editor'
+
+function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+
+  return (
+    <>
+      <div className="pane top-pane">
+      <Editor
+          language = 'xml'
+          displayName = "HTML"
+          value = {html}
+          onChange = {setHtml}
+        />
+        <Editor
+          language = 'css'
+          displayName = "CSS"
+          value = {css}
+          onChange = {setCss}
+        />
+        <Editor
+          language = 'javascript'
+          displayName = "Javascript"
+          value = {js}
+          onChange = {setJs}
+        />
+        
+      </div>
+      <div className="pane">
+        <iframe title='output' sandbox='allow-scripts'  width="100%" height="100%" frameborder="0"/>
+      </div>
+    </>
+  )
+}
+
+export default App

980ad20af37709e68d76916734a3b5ffe46618b5
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-26 20:10:44 +0500

    Code Editor

--- codePen/src/components/App.jsx
+++ codePen/src/components/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 function App() {
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJs] = useState('')
+  const [html, setHtml] = useLocalStorage('html','')
+  const [css, setCss] = useLocalStorage('css','')
+  const [js, setJs] = useLocalStorage('js','')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+          <html>
+            <body>${html}</body>
+            <style>${css}</style>
+            <script>${js}</script>
+          </html>
+        `)
+    }, 250);
+  }, [html, css, js])
+  
 
   return (
     <>
@@ -30,7 +45,14 @@ function App() {
         
       </div>
       <div className="pane">
-        <iframe title='output' sandbox='allow-scripts'  width="100%" height="100%" frameborder="0"/>
+        <iframe 
+        srcDoc={srcDoc}
+        title='output' 
+        sandbox='allow-scripts'  
+        frameBorder = "0"
+        width="100%" 
+        height="100%" 
+        />
       </div>
     </>
   )
--- codePen/src/components/Editor.jsx
+++ codePen/src/components/Editor.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faE, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 function Editor(props) {
     const {
@@ -14,13 +16,19 @@ function Editor(props) {
         onChange
     } = props
 
+    const [open, setOpen] = useState(true)
     function handleChange(editor, data, value) {
         onChange(value)
     }
     return (
-        <div className='editor-container'>
+        <div className={`editor-container ${open ? '': 'collapsed'}`}>
             <div className="editor-title">{displayName}
-            <button>O/C</button>
+            <button 
+            type='button'
+            className='expand-collapse-btn'
+            onClick={() => setOpen(openPrev => !openPrev)}>
+                <FontAwesomeIcon icon = {open ? faCompressAlt: faExpandAlt} />
+                </button>
             </div>
             <ControlledEditor
                 onBeforeChange={handleChange}

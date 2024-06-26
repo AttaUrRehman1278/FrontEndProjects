3471b93b4b9b67e2493b8ab54e1b330317232f7a
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-27 01:20:20 +0500

    Youtube Clone

---
+++ YoutubeClone/src/firebase.jsx
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCgR61zPnB5V0dul1Zgxqz9-AHSzXo-QCM",
+    authDomain: "clone-cb8d1.firebaseapp.com",
+    projectId: "clone-cb8d1",
+    storageBucket: "clone-cb8d1.appspot.com",
+    messagingSenderId: "976431186664",
+    appId: "1:976431186664:web:7d48c32b65c21ea76d7f5d",
+    measurementId: "G-EW3Y6F2VLM"
+};

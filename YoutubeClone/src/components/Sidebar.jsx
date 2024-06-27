791672af4d562df4225b548a934a165191d24bb5
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-27 16:49:24 +0500

    Youtube Clone

--- YoutubeClone/src/components/Sidebar.jsx
+++ YoutubeClone/src/components/Sidebar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./Sidebar.css"
+import SidebarRow from './SidebarRow'
+import { HomeSharp } from '@mui/icons-material'
+import { Whatshot } from '@mui/icons-material'
+import { Subscriptions } from '@mui/icons-material'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>I am a Sidebar</h1>
+            <SidebarRow Icon = {HomeSharp} title = "Home"/>
+            <SidebarRow Icon = {Whatshot} title = "Trending"/>
+            <SidebarRow Icon = {Subscriptions} title = "Subscription"/>
         </div>
     )
 }

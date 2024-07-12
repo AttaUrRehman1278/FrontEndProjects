199c5e2f288c973825677dad653daa624abdf2cf
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-07-12 14:17:02 +0500

    Spotify

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,8 +1,28 @@
-console.log("This is javascript file")
+console.log("This is javascript file");
 
 
 async function getSurahs() {
-    let a = await fetch("http://127.0.0.1:3000/tilawat/")
-    let response = await a.text()
-    console.log(response)
+    let a = await fetch("http://127.0.0.1:3000/tilawat/");
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let surahs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            surahs.push(element.href)
+        }
+    }
+    return surahs
 }
+
+async function main() {
+
+    let surahs = await getSurahs();
+    console.log(surahs);
+    var audio = new Audio(surahs[0]);
+    // audio.play();
+}
+
+main()

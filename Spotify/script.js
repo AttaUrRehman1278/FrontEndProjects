d8aeef470e87c779b0aa8bf6e10b324316bee058
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-09-04 23:28:15 +0500

    change

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,94 +1,98 @@
 console.log("This is javascript file");
 
-let currentSurah = new Audio()
+let currentSurah = new Audio();
 
-function SecondsToMinSec(seconds) {
+function secondsToMinSec(seconds) {
     const minutes = Math.floor(seconds / 60);
     const secs = Math.floor(seconds % 60);
-    return minutes.toString().padStart(2, '0') + ':' +
-        secs.toString().padStart(2, '0');
+    return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 }
+
 async function getSurahs() {
-    let a = await fetch("http://127.0.0.1:3000/tilawat/");
-    let response = await a.text();
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
-    let surahs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            surahs.push(element.href.split("/tilawat/")[1])
-        }
-    }
-    return surahs
+    const response = await fetch("http://127.0.0.1:3000/tilawat/");
+    const text = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, 'text/html');
+    const links = doc.querySelectorAll('a[href$=".mp3"]');
+    return Array.from(links).map(link => link.href.split("/tilawat/")[1]);
 }
 
-const playAudio = (track, pause=false) => {
-    currentSurah.src = "/tilawat/" + track
-    if (!pause){
-        currentSurah.play()
+const playAudio = (track, pause = false) => {
+    currentSurah.src = "/tilawat/" + track;
+    if (!pause) {
+        currentSurah.play();
     }
-    play.src = "public/pause.svg"
-    document.querySelector(".surahinfo").innerHTML = decodeURI(track.split(".")[0])
+    play.src = "public/pause.svg";
+    document.querySelector(".surahinfo").textContent = decodeURI(track.split(".")[0]);
 }
+
 async function main() {
+    const surahs = await getSurahs();
+    playAudio(surahs[0], true);
 
-    let surahs = await getSurahs();
-    playAudio(surahs[0], true)
-    let surahUL = document.querySelector(".playlist").getElementsByTagName("ul")[0];
-    for (const surah of surahs) {
-        surahUL.innerHTML = surahUL.innerHTML + `<li> <img src="public/rockstar-umair.jfif" alt="">
+    const surahUL = document.querySelector(".playlist ul");
+    const surahCard = document.querySelector(".album-cards");
+
+    const createSurahList = (surah) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <img src="public/rockstar-umair.jfif" alt="">
             <div class='info'>
-                <div class= 'song-name'>${surah.replaceAll("%20", " ")} </div>
+                <div class='song-name'>${surah.replaceAll("%20", " ")}</div>
             </div>
-            <div class = 'play-song'>
+            <div class='play-song'>
                 <img src="public/play.svg" alt="">
             </div>
-        </li>`
-    }
-    
-    Array.from(document.querySelector(".playlist").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", element => {
-            playAudio(e.querySelector(".song-name").innerHTML)
-        })
-    })
+        `;
+        li.addEventListener("click", () => playAudio(surah));
+        return li;
+    };
 
-    let surahCard = document.querySelector(".album-cards");
-    for (const surah of surahs){
-        surahCard.innerHTML = surahCard.innerHTML + `<div class="card">
+    const createSurahCard = (surah) => {
+        const div = document.createElement('div');
+        div.className = 'card';
+        div.innerHTML = `
             <img src="public/rockstar-umair.jfif" alt="">
             <h3 class="surah-name">${surah.replaceAll("%20", " ")}</h3>
-        </div>`
-    }
-    Array.from(document.querySelector(".album-cards").getElementsByClassName("card")).forEach(e => {
-        e.addEventListener("click", element => {
-            playAudio(e.querySelector(".surah-name").innerHTML)
-        })
-    })
+        `;
+        div.addEventListener("click", () => playAudio(surah));
+        return div;
+    };
 
-    play.addEventListener("click", ()=> {
+    const fragment1 = document.createDocumentFragment();
+    const fragment2 = document.createDocumentFragment();
+
+    surahs.forEach(surah => {
+        fragment1.appendChild(createSurahList(surah));
+        fragment2.appendChild(createSurahCard(surah));
+    });
+
+    surahUL.appendChild(fragment1);
+    surahCard.appendChild(fragment2);
+
+    play.addEventListener("click", () => {
         if (currentSurah.paused) {
-            currentSurah.play()
-            play.src = "public/pause.svg"
+            currentSurah.play();
+            play.src = "public/pause.svg";
+        } else {
+            currentSurah.pause();
+            play.src = "public/play.svg";
         }
-        else {
-            currentSurah.pause()
-            play.src = "public/play.svg"
-        }
-    })
+    });
 
-    currentSurah.addEventListener("timeupdate", ()=>{
-        console.log(currentSurah.currentTime, currentSurah.duration)
-        document.querySelector(".surahtime").innerHTML = `${SecondsToMinSec(currentSurah.currentTime)}/${SecondsToMinSec(currentSurah.duration)}`
-        document.querySelector(".circle").style.left = currentSurah.currentTime / currentSurah.duration * 100 + "%"
-    })
-    
-    document.querySelector(".seekbar").addEventListener("click", (e)=>{
-        let percent = e.offsetX / document.querySelector(".seekbar").offsetWidth * 100;
-        document.querySelector(".circle").style.left = percent + "%"
-        currentSurah.currentTime = percent / 100 * currentSurah.duration
-    })
+    const surahTime = document.querySelector(".surahtime");
+    const circle = document.querySelector(".circle");
+
+    currentSurah.addEventListener("timeupdate", () => {
+        surahTime.textContent = `${secondsToMinSec(currentSurah.currentTime)}/${secondsToMinSec(currentSurah.duration)}`;
+        circle.style.left = `${(currentSurah.currentTime / currentSurah.duration) * 100}%`;
+    });
+
+    document.querySelector(".seekbar").addEventListener("click", (e) => {
+        const percent = e.offsetX / e.currentTarget.offsetWidth;
+        circle.style.left = `${percent * 100}%`;
+        currentSurah.currentTime = percent * currentSurah.duration;
+    });
 }
 
-main()
+main();
